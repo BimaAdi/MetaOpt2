75366e0137650851805f1ae9c359b0e116655ad6
Bima <bimaadi419@gmail.com>
2019-02-25 09:03:58 +0700

    Add CS Algorithm (Done)

--- R/CS.Algorithm.R
+++ R/CS.Algorithm.R
@@ -1,110 +1,79 @@
 # Cuckoo Search (CS)
-require("rmutil")
+
 source('./R/metaheuristic.FunctionCollection.R')
 
-FUN <- sphere
-optimType <- "MIN"
-numPopulation <- 25
-numVar <- 1
-maxIter <- 500
-rangeVar <- matrix(c(-10, 10), nrow = 2)
-stepSize <- 1
-abandonedFraction <- 0.5
-
-# calculate the dimension of problem if not specified by user
-dimension <- ncol(rangeVar)
-
-# parsing rangeVar to lowerBound and upperBound
-lowerBound <- rangeVar[1,]
-upperBound <- rangeVar[2,]
-
-# if user define the same upper bound and lower bound for each dimension
-if(dimension==1){
-  dimension <- numVar
-}
-
-## convert optimType to numerical form
-## 1 for minimization and -1 for maximization
-if(optimType == "MAX") optimType <- -1 else optimType <- 1
-
-# if user only define one lb and ub, then repeat it until the dimension
-if(length(lowerBound)==1 & length(upperBound)==1){
-  lowerBound <- rep(lowerBound, dimension)
-  upperBound <- rep(upperBound, dimension)
-}
-
-#generate candidate solutions
-candidateSolution <- generateRandom(numPopulation, dimension, lowerBound, upperBound)
-
-fitness <- calcFitness(FUN, optimType, candidateSolution)
-candidateSolutions <- data.frame(candidateSolution, fitness)
-indexVariable <- 1:numVar
-numRemove <- round(numPopulation*abandonedFraction)
-
-for(t in 1:maxIter){
-  #print(t)
-  # move a candidate solution randomly as cuckoo egg
-  choosenCS <- sample(1:numPopulation, 1)
-  choosenCS <- as.matrix(candidateSolutions[choosenCS, indexVariable])
-  levyRandom <- rlevy(n=numVar)
-  levyRandom[levyRandom > 3] <- 3
-  levyRandom <- matrix(levyRandom, ncol = numVar)
-  cuckoo <- choosenCS + stepSize * levyRandom
+CS <- function(FUN, optimType="MIN", numVar, numPopulation=40, maxIter=500, rangeVar, abandonedFraction=0.5){
+  # calculate the dimension of problem if not specified by user
+  dimension <- ncol(rangeVar)
   
-  # pick a candidate solution randomly and compare it with cuckoo egg
-  pickedCSIndex <- sample(1:numPopulation, 1)
-  if(candidateSolutions[pickedCSIndex, "fitness"] > calcFitness(FUN, optimType, as.matrix(cuckoo))){
-    candidateSolutions[pickedCSIndex, indexVariable] <- as.matrix(cuckoo)
-    candidateSolutions[pickedCSIndex, "fitness"] <- calcFitness(FUN, optimType, as.matrix(cuckoo))
+  # parsing rangeVar to lowerBound and upperBound
+  lowerBound <- rangeVar[1,]
+  upperBound <- rangeVar[2,]
+  
+  # if user define the same upper bound and lower bound for each dimension
+  if(dimension==1){
+    dimension <- numVar
   }
   
-  # remove fraction (abandonedFraction) from candidate solutions with worst fitness
-  candidateSolutions <- candidateSolutions[order(candidateSolutions$fitness),]
-  removeCSIndex <- (numPopulation - numRemove + 1):numPopulation
-  bestVar <- as.numeric(candidateSolutions[order(candidateSolutions$fitness)[1],indexVariable])
-  candidateSolutions[removeCSIndex, indexVariable] <- generateRandomCS(numRemove, dimension, bestVar, stepSize, lowerBound, upperBound)
-  candidateSolutions$fitness <- calcFitness(FUN, optimType, as.matrix(candidateSolutions[,indexVariable]))
-  candidateSolutions <- candidateSolutions[order(candidateSolutions$fitness),]
+  ## convert optimType to numerical form
+  ## 1 for minimization and -1 for maximization
+  if(optimType == "MAX") optimType <- -1 else optimType <- 1
+  
+  # if user only define one lb and ub, then repeat it until the dimension
+  if(length(lowerBound)==1 & length(upperBound)==1){
+    lowerBound <- rep(lowerBound, dimension)
+    upperBound <- rep(upperBound, dimension)
+  }
+  
+  #generate candidate solutions
+  candidateSolution <- generateRandom(numPopulation, dimension, lowerBound, upperBound)
+  bestPos <- engineCS(FUN, optimType, maxIter, lowerBound, upperBound, candidateSolution, abandonedFraction)
+  
+  return(bestPos)
 }
 
-generateRandomCS <- function(numRemove, numVar, bestVar, stepSize, lowerBound, upperBound){
-  numLocalSearch <- ceiling(numVar/2)
-  numPureRandom <- numRemove - numLocalSearch
-  # localSearch
-  levyRandom <- rlevy(n=numVar*numLocalSearch)
-  levyRandom[levyRandom > 3] <- 3
-  levyRandom <- matrix(levyRandom, ncol = numVar)
-  localSearch <- t(bestVar + stepSize * t(levyRandom))
-  # pureRandom
-  pureRandom <- generateRandom(numPureRandom, numVar, lowerBound, upperBound)
-  result <- as.matrix(rbind(localSearch, pureRandom))
-  return(result)
-}
-# -------------------------------------------------------------------
-progressbar <- txtProgressBar(min = 0, max = maxIter, style = 3)
-for(t in 1:maxIter){
-  for(index in 1:numPopulation){
-    # move a candidate solution randomly as cuckoo egg
-    choosenCS <- as.matrix(candidateSolutions[index, indexVariable])
-    levyRandom <- rlevy(n=numVar)
-    levyRandom[levyRandom > 3] <- 3
-    levyRandom <- matrix(levyRandom, ncol = numVar)
-    cuckoo <- choosenCS + stepSize * levyRandom
+engineCS <- function(FUN, optimType, maxIter, lowerBound, upperBound, candidateSolution, abandonedFraction){
+  numPopulation <- nrow(candidateSolution)
+  numVar <- ncol(candidateSolution)
+  best <- c()
+  
+  progressbar <- txtProgressBar(min = 0, max = maxIter, style = 3)
+  for(t in 1:maxIter){
+    best <- calcBest(FUN, -1*optimType, rbind(candidateSolution, best))
+    # create a cuckoo egg
+    choosen <- sample.int(numPopulation, 1)
+    choosen <- candidateSolution[choosen,]
+    cuckooegg <- choosen + levyFlight(choosen, best)
     
-    # pick a candidate solution randomly and compare it with cuckoo egg
-    pickedCSIndex <- sample(1:numPopulation, 1)
-    if(candidateSolutions[pickedCSIndex, "fitness"] > calcFitness(FUN, optimType, as.matrix(cuckoo))){
-      candidateSolutions[pickedCSIndex, indexVariable] <- as.matrix(cuckoo)
-      candidateSolutions[pickedCSIndex, "fitness"] <- calcFitness(FUN, optimType, as.matrix(cuckoo))
+    # compare with candidate solution
+    otherChoosen <- sample.int(numPopulation, 1)
+    otherFitness <- calcFitness(FUN, optimType, matrix(candidateSolution[otherChoosen, ], ncol = numVar))
+    cuckooFitness <- calcFitness(FUN, optimType, matrix(cuckooegg, ncol = numVar))
+    if(cuckooFitness < otherFitness){
+      candidateSolution[otherChoosen, ] <- cuckooegg
     }
     
-    # remove fraction (abandonedFraction) from candidate solutions with worst fitness
-    candidateSolutions <- candidateSolutions[order(candidateSolutions$fitness),]
-    removeCSIndex <- (numPopulation - numRemove + 1):numPopulation
-    candidateSolutions[removeCSIndex, indexVariable] <- generateRandom(numRemove, dimension, lowerBound, upperBound)
-    candidateSolutions$fitness <- calcFitness(FUN, optimType, as.matrix(candidateSolutions[,indexVariable]))
+    # remove fraction
+    CSFitness <- calcFitness(FUN, optimType, candidateSolution)
+    candidateSolution <- as.matrix(candidateSolution[order(CSFitness), ])
+    numCSToRemove <- round(numPopulation * abandonedFraction)
+    removeIndex <- as.numeric((numPopulation - numCSToRemove + 1):numPopulation)
+    candidateSolution[removeIndex,] <- generateRandom(numCSToRemove, numVar, lowerBound, upperBound)
+    setTxtProgressBar(progressbar, t)
   }
-  setTxtProgressBar(progressbar, t)
+  close(progressbar)
+  best <- calcBest(FUN, -1*optimType, rbind(candidateSolution, best))
+  return(best)  
 }
-close(progressbar)
 
+levyFlight <- function(CS, best){
+  n <- length(CS)
+  beta <- 3/2
+  sigma <- (gamma(1+beta)*sin(pi*beta/2)/(gamma((1+beta)/2)*beta*2^((beta-1)/2)))^(1/beta)
+  u <- rnorm(n) * sigma
+  v <- rnorm(n)
+  step <- u/abs(v)^(1/beta)
+  stepsize <- 0.01 * step * (CS - best)
+  stepsize <- stepsize * rnorm(n) 
+  return(stepsize)
+}
