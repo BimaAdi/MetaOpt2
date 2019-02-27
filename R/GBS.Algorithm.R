4ed4c774a3089307d18b8337cd2484e215e52351
Bima <bimaadi419@gmail.com>
2019-02-27 21:55:54 +0700

    Add GBS Algorithm and KH Algorithm (Done)

---
+++ R/GBS.Algorithm.R
@@ -0,0 +1,87 @@
+# Gravitational Based Search Algorithm (GBS)
+
+source('./R/metaheuristic.FunctionCollection.R')
+
+GBS <- function(FUN, optimType="MIN", numVar, numPopulation=40, maxIter=500, rangeVar,
+                gravitationalConst=100, kbest=0.5){
+  # calculate the dimension of problem if not specified by user
+  dimension <- ncol(rangeVar)
+  
+  # parsing rangeVar to lowerBound and upperBound
+  lowerBound <- rangeVar[1,]
+  upperBound <- rangeVar[2,]
+  
+  # if user define the same upper bound and lower bound for each dimension
+  if(dimension==1){
+    dimension <- numVar
+  }
+  
+  ## convert optimType to numerical form
+  ## 1 for minimization and -1 for maximization
+  if(optimType == "MAX") optimType <- -1 else optimType <- 1
+  
+  # generate initial population
+  candidateSolution <- generateRandom(numPopulation, dimension, lowerBound, upperBound)
+  bestPos <- engineGBS(FUN, optimType, maxIter, lowerBound, upperBound, candidateSolution, 
+                       gravitationalConst, kbest)
+  return(bestPos)
+}
+
+engineGBS <- function(FUN, optimType, maxIter, lowerBound, upperBound, candidateSolution,
+                      gravitationalConst=100, kbest=0.5){
+  # GBS starthere ----
+  numVar <- ncol(candidateSolution)
+  numPopulation <- nrow(candidateSolution)
+  # give every candidate solution initial velocity
+  velocity <- matrix(rep(0, numPopulation * numVar), ncol = numVar)
+  
+  gbest <- calcBest(FUN, -1*optimType, candidateSolution)
+  progressbar <- txtProgressBar(min = 0, max = maxIter, style = 3)
+  for(t in 1:maxIter){
+    # update gravitational constant
+    eulerConst <- 0.5772156649
+    gravitationalConstT <- gravitationalConst / exp(0.01 * t)
+    # get best and worst candidate solution in this current t
+    CSFitness <- calcFitness(FUN, optimType, candidateSolution)
+    best <- candidateSolution[order(CSFitness)[1],]
+    worst <- candidateSolution[order(CSFitness)[numPopulation],]
+    bestFitness <- calcFitness(FUN, optimType, matrix(best, ncol = numVar))
+    worstFitness <- calcFitness(FUN, optimType, matrix(worst, ncol = numVar))
+    
+    # calculate gravitaional mass for each candidate solution
+    totalGM <- sum((CSFitness - worstFitness) / (bestFitness - worstFitness))
+    if(is.nan(totalGM)){
+      print("out of bound")
+      break
+    }
+    GM <- (CSFitness - worstFitness) / (bestFitness - worstFitness)
+    GM <- GM / totalGM
+    
+    # calculate total force
+    epsilon <- 8.854e-12
+    k <- round(numPopulation * kbest)
+    candidateSolution <- as.matrix(candidateSolution[as.numeric(order(CSFitness)), ])
+    velocity <- as.matrix(velocity[as.numeric(order(CSFitness)), ])
+    totalForce <- c()
+    for(i in 1:numPopulation){
+      totalI <- rep(0, numVar)
+      for(j in 1:k){
+        distance <- as.numeric(dist(rbind(candidateSolution[i,],candidateSolution[j,])))
+        Force <- runif(1) * gravitationalConstT * GM[j] * (candidateSolution[j,] - candidateSolution[i, ])/distance
+        if(all(is.nan(Force))){
+          Force <- as.numeric(rep(0, numVar))
+        }
+        totalI <- totalI + Force
+      }
+      totalForce <- rbind(totalForce, totalI)
+    }
+    
+    randomMatrix <- matrix(runif(numPopulation * numVar), ncol = numVar)
+    velocity <- randomMatrix * velocity + totalForce
+    candidateSolution <- candidateSolution + velocity
+    gbest <- calcBest(FUN, -1*optimType, rbind(candidateSolution, gbest))
+    setTxtProgressBar(progressbar, t)
+  }
+  close(progressbar)
+  return(gbest)  
+}
--- R/KH.Algorithm.R
+++ R/KH.Algorithm.R
@@ -2,202 +2,184 @@
 
 source('./R/metaheuristic.FunctionCollection.R')
 
-FUN <- sphere
-optimType <- "MIN"
-numVar <- 1
-numPopulation <- 10
-maxIter <- 3
-rangeVar <- matrix(c(-10, 10), nrow = 2)
-maxMotionInduced <- 0.01
-inertiaWeightOfMotionInduced <- 0.5
-epsilon <- 5e-05
-foragingSpeed <- 0.02
-inertiaWeightOfForagingSpeed <- 0.2 #[0, 1]
-maxDifussionSpeed <- 0.001 # [0.002, 0.01]
-constantSpace <- 0.1 #[0, 2]
-mu <- 0.5 #[0, 1]
-
-# calculate the dimension of problem if not specified by user
-dimension <- ncol(rangeVar)
-
-# parsing rangeVar to lowerBound and upperBound
-lowerBound <- rangeVar[1,]
-upperBound <- rangeVar[2,]
-
-# if user define the same upper bound and lower bound for each dimension
-if(dimension==1){
-  dimension <- numVar
+KH <- function(FUN, optimType="MIN", numVar, numPopulation=40, maxIter=500, rangeVar,
+               maxMotionInduced=0.01, inertiaWeightOfMotionInduced=0.01, epsilon=5e-05, foragingSpeed=0.02,
+               inertiaWeightOfForagingSpeed=0.01, maxDifussionSpeed=0.01, constantSpace=1, mu=0.5){
+  # calculate the dimension of problem if not specified by user
+  dimension <- ncol(rangeVar)
+  
+  # parsing rangeVar to lowerBound and upperBound
+  lowerBound <- rangeVar[1,]
+  upperBound <- rangeVar[2,]
+  
+  # if user define the same upper bound and lower bound for each dimension
+  if(dimension==1){
+    dimension <- numVar
+  }
+  
+  ## convert optimType to numerical form
+  ## 1 for minimization and -1 for maximization
+  if(optimType == "MAX") optimType <- -1 else optimType <- 1
+  
+  # if user only define one lb and ub, then repeat it until the dimension
+  if(length(lowerBound)==1 & length(upperBound)==1){
+    lowerBound <- rep(lowerBound, dimension)
+    upperBound <- rep(upperBound, dimension)
+  }
+  
+  # generate candidate solution
+  candidateSolution <- generateRandom(numPopulation, dimension, lowerBound, upperBound)
+  bestPos <- engineKH(FUN, optimType, maxIter, lowerBound, upperBound, candidateSolution,
+                      maxMotionInduced, inertiaWeightOfMotionInduced, epsilon, foragingSpeed,
+                      inertiaWeightOfForagingSpeed, maxDifussionSpeed, constantSpace, mu)
+  return(bestPos)
 }
 
-## convert optimType to numerical form
-## 1 for minimization and -1 for maximization
-if(optimType == "MAX") optimType <- -1 else optimType <- 1
-
-# if user only define one lb and ub, then repeat it until the dimension
-if(length(lowerBound)==1 & length(upperBound)==1){
-  lowerBound <- rep(lowerBound, dimension)
-  upperBound <- rep(upperBound, dimension)
-}
-
-# generate candidate solution
-candidateSolution <- generateRandom(numPopulation, dimension, lowerBound, upperBound)
-
-# generate candidate solutions
-indexVariable <- 1:numVar
-fitness <- calcFitness(FUN, optimType, candidateSolution)
-motionInduced <- matrix(rep(0, numPopulation * numVar), ncol = numVar)
-foragingMotion <- matrix(rep(0, numPopulation * numVar), ncol = numVar)
-physicalDifusion <- matrix(rep(0, numPopulation * numVar), ncol = numVar) 
-candidateSolutions <- data.frame(candidateSolution, fitness)
-candidateSolutions$fitness <- calcFitness(FUN, optimType, as.matrix(candidateSolutions[,1:numVar]))
-best <- candidateSolutions[order(candidateSolutions$fitness)[1],]
-worst <- candidateSolutions[order(candidateSolutions$fitness)[numPopulation],]
-
-for(t in 1:maxIter){
-  print(t)
-  # Motion Induced Calculation ----
-  # alpha local
-  sensingDistance <- isIncludeSensingDistances(candidateSolutions)
-  alphaLocal <- c()
-  for(index in 1:numPopulation){
-    # motion calculation
-    jVariable <- as.matrix(candidateSolutions[sensingDistance[index,], indexVariable])
-    iVariable <- as.numeric(candidateSolutions[index, indexVariable])
-    motion <- KHMotion(iVariable, jVariable, epsilon)
+engineKH <- function(FUN, optimType, maxIter, lowerBound, upperBound, candidateSolution,
+                     maxMotionInduced, inertiaWeightOfMotionInduced, epsilon, foragingSpeed,
+                     inertiaWeightOfForagingSpeed, maxDifussionSpeed, constantSpace, mu){
+  numVar <- ncol(candidateSolution)
+  numPopulation <- nrow(candidateSolution)
+  
+  N <- matrix(rep(0, numPopulation * numVar), ncol = numVar) 
+  f <- matrix(rep(0, numPopulation * numVar), ncol = numVar)   
+  
+  gbest <- calcBest(FUN, -1*optimType, candidateSolution)
+  progressbar <- txtProgressBar(min = 0, max = maxIter, style = 3)
+  for(t in 1:maxIter){
+    CSFitness <- calcFitness(FUN, optimType, candidateSolution)
+    best <- candidateSolution[order(CSFitness)[1], ]
+    worst <- candidateSolution[order(CSFitness)[numPopulation], ]
+    bestFitness <- calcFitness(FUN, optimType, matrix(best, ncol = numVar))
+    worstFitness <- calcFitness(FUN, optimType, matrix(worst, ncol = numVar))
+    gbest <- calcBest(FUN, -1*optimType, rbind(candidateSolution, gbest))
     
-    # fitness calculation
-    jFitness <- as.matrix(candidateSolutions[sensingDistance[index, ], "fitness"])
-    iFitness <- candidateSolutions[index, "fitness"]
-    fitness <- KHFitnessMotion(iFitness, jFitness, worst$fitness, best$fitness)
+    # motion iduced
+    sensingDistance <- 1/5*ncol(candidateSolution)*colSums(as.matrix(dist(candidateSolution)))
+    isIncludeSD <- as.matrix(dist(candidateSolution, diag = T, upper = T)) < sensingDistance
+    candidateSolution[isIncludeSD[1,],]
     
-    # alpha local calculation
-    alphaLocal <- rbind(alphaLocal, colSums(fitness * motion))
-  }
-  
-  random <- randomMatrix(numPopulation, numVar, 0, 1)
-  # as.numeric ??
-  KHMotionCSBest <- KHMotion(as.matrix(candidateSolutions[,indexVariable]), as.numeric(best[,indexVariable]), epsilon)
-  KHFitnessMotionCSBest <- KHFitnessMotion(candidateSolutions[,"fitness"], best$fitness, worst$fitness, best$fitness)
-  motionInduced <- maxMotionInduced * (alphaLocal + 2 * (random + t/maxIter) * KHFitnessMotionCSBest * KHMotionCSBest) + 
-    inertiaWeightOfMotionInduced * motionInduced
-  
-  # foraging motion calculation ----
-  if(numVar == 1){
-    food <- sum(1 / candidateSolutions$fitness * candidateSolutions[,indexVariable])/sum(1 / candidateSolutions$fitness)
-  }else{
-    food <- colSums(1 / candidateSolutions$fitness * candidateSolutions[,indexVariable])/sum(1 / candidateSolutions$fitness)
-  }
-  foodFitness <- calcFitness(FUN, optimType, t(food))
-  betaFood <- KHFitnessMotion(candidateSolutions[,"fitness"], foodFitness, worst$fitness, best$fitness) * 
-    KHMotion(as.matrix(candidateSolutions[,indexVariable]), food, epsilon) 
-  betaBest <- KHFitnessMotion(candidateSolutions[,"fitness"], best$fitness, worst$fitness, best$fitness) *
-    KHMotion(as.matrix(candidateSolutions[,indexVariable]), best[,indexVariable], epsilon)
-  foragingMotion <- foragingSpeed * (2*(1 - t/maxIter) * betaFood * betaBest) + inertiaWeightOfForagingSpeed * foragingMotion
-  
-  # Physical Difusion Calculation ----
-  randomDirectionalVector <- randomMatrix(numPopulation, numVar, -1, 1)
-  physicalDifusion[,] <- maxDifussionSpeed * (1 - t/maxIter) * randomDirectionalVector
-  
-  # calculate motion calculation & update candidate solutions ----
-  motionCalculation <- motionInduced + foragingMotion + physicalDifusion
-  deltaT <- constantSpace * sum(upperBound - lowerBound)
-  candidateSolutions[,indexVariable] <- candidateSolutions[,indexVariable] + deltaT * motionCalculation
-  candidateSolutions$fitness <- calcFitness(FUN, optimType, as.matrix(candidateSolutions[,1:numVar]))
-  best <- candidateSolutions[order(candidateSolutions$fitness)[1],]
-  worst <- candidateSolutions[order(candidateSolutions$fitness)[numPopulation],]
-  
-  # implemented genetic operator crossover
-  idBest <- order(candidateSolutions$fitness)[1]
-  print(KHFitnessMotion(candidateSolutions[-1*idBest,"fitness"], best$fitness, worst$fitness, best$fitness))
-  Cr <- 0.2 * KHFitnessMotion(candidateSolutions[-1*idBest,"fitness"], best$fitness, worst$fitness, best$fitness)
-  print(Cr)
-  Cr <- matrix(as.numeric(rep(Cr, numVar)), ncol = numVar)
-  random <- randomMatrix(numPopulation - 1, numVar, 0, 1)
-  probCr <- random < Cr
-  if(!all(probCr == FALSE)){
-    print("in probCr")
-    CrCandidateSolutions <- as.matrix(candidateSolutions[-1*idBest, indexVariable])
-    CrCandidateSolutions[probCr] <- sapply(col(probCr)[probCr], function(x){
-      choosenCS <- sample(1:(numPopulation - 1), 1)
-      return(CrCandidateSolutions[choosenCS, x])
+    alpha <- c()
+    for(index in 1:numPopulation){
+      X <- apply(as.matrix(candidateSolution[isIncludeSD[index,],]), c(1), function(x, y){
+        xij(y, x, epsilon)
+      }, y=candidateSolution[index,])
+      
+      K <- sapply(CSFitness[isIncludeSD[index,]], function(x, y){
+        kij(y,x, bestFitness, worstFitness)
+      }, y=CSFitness[index])
+      
+      if(numVar == 1){
+        alphaLocal <- sum(X * K)
+      }else{
+        alphaLocal <- colSums(t(X) * K)
+      }
+      
+      Cbest <- 2*(runif(1)+t/maxIter)
+      X <- xij(candidateSolution[index,], best, epsilon)
+      K <- kij(CSFitness[index], bestFitness, bestFitness, worstFitness)
+      alphaTarget <- Cbest*K*X
+      alpha <- rbind(alpha, alphaLocal + alphaTarget)  
+    }
+    
+    N <- maxMotionInduced * alpha + inertiaWeightOfMotionInduced * N
+    
+    # foraging motion
+    if(numVar == 1){
+      Xfood  <- sum(candidateSolution * 1 / CSFitness) / sum(1/CSFitness)
+    }else{
+      Xfood  <- colSums(candidateSolution * 1 / CSFitness) / sum(1/CSFitness)
+    }
+    xFoodFitness <- calcFitness(FUN, optimType, matrix(Xfood, ncol = numVar))
+    Cfood <- 2*(1-t/maxIter)
+    Kifood <- sapply(CSFitness, function(x){
+      kij(x, xFoodFitness, bestFitness, worstFitness)  
     })
-    candidateSolutions[-1*idBest, indexVariable] <- CrCandidateSolutions
-    candidateSolutions$fitness <- calcFitness(FUN, optimType, as.matrix(candidateSolutions[,1:numVar]))
-    best <- candidateSolutions[order(candidateSolutions$fitness)[1],]
-    worst <- candidateSolutions[order(candidateSolutions$fitness)[numPopulation],]
-  }
-  
-  # implemented genetic operator mutation
-  idBest <- order(candidateSolutions$fitness)[1]
-  Mu <- 0.05/KHFitnessMotion(candidateSolutions[-1*idBest,"fitness"], best$fitness, worst$fitness, best$fitness)
-  Mu <- matrix(as.numeric(rep(Mu, numVar)), ncol = numVar)
-  random <- randomMatrix(numPopulation - 1, numVar, 0, 1)
-  probMu <- random < Mu
-  if(!all(probMu == FALSE)){
-    MuCandidateSolutions <- as.matrix(candidateSolutions[-1*idBest, indexVariable])
-    MuCandidateSolutions[probMu] <- sapply(col(probMu)[probMu], function(x){
-      bestVariable <- best[,indexVariable][,x]
-      item <- 1:(numPopulation - 1)
-      p <- MuCandidateSolutions[sample(item, 1), x]
-      q <- MuCandidateSolutions[sample(item, 1), x]
-      return(bestVariable + mu*(p - q))
+    Xifood <- apply(candidateSolution, c(1), function(x){
+      xij(x, Xfood, epsilon)
     })
-    candidateSolutions[-1*idBest, indexVariable] <- MuCandidateSolutions
-    candidateSolutions$fitness <- calcFitness(FUN, optimType, as.matrix(candidateSolutions[,1:numVar]))
-    best <- candidateSolutions[order(candidateSolutions$fitness)[1],]
-    worst <- candidateSolutions[order(candidateSolutions$fitness)[numPopulation],]
+    
+    Kibest <- sapply(CSFitness, function(x){
+      kij(x, bestFitness, bestFitness, worstFitness)  
+    })
+    Xibest <- apply(candidateSolution, c(1), function(x){
+      xij(x, best, epsilon)
+    })
+    
+    if(numVar == 1){
+      betaFood <- Cfood*Kifood*Xifood
+      betaBest <- Xibest * Kibest
+    }else{
+      betaFood <- t(Cfood*Kifood*Xifood)
+      betaBest <- t(Xibest) * Kibest
+    }
+    beta <- betaFood + betaBest
+    f <- foragingSpeed*beta + inertiaWeightOfForagingSpeed*f
+    
+    # physical difussion
+    D <- maxDifussionSpeed * (1 - t/maxIter)*runif(1, min = -1, max = 1)
+    
+    # Motion calculation
+    TotalMotion <- N + f + D
+    deltaT <- constantSpace*sum(upperBound - lowerBound)
+    candidateSolution <- candidateSolution + deltaT * TotalMotion
+    
+    # implement genetic operator ----
+    
+    # CrossOver
+    CSFitness <- calcFitness(FUN, optimType, candidateSolution)
+    best <- candidateSolution[order(CSFitness)[1], ]
+    worst <- candidateSolution[order(CSFitness)[numPopulation], ]
+    bestFitness <- calcFitness(FUN, optimType, matrix(best, ncol = numVar))
+    worstFitness <- calcFitness(FUN, optimType, matrix(worst, ncol = numVar))
+    gbest <- calcBest(FUN, -1*optimType, rbind(candidateSolution, gbest))
+    Kibest <- sapply(CSFitness, function(x){
+      kij(x, bestFitness, bestFitness, worstFitness)  
+    })
+    randomMatrix <- matrix(runif(numVar * numPopulation), ncol = numVar)
+    Cr <- 0.2 * Kibest
+    prob <- randomMatrix < Cr
+    if(!all(prob == FALSE)){
+      Xrm <- sapply(col(candidateSolution)[prob], function(x){
+        choosen <- sample.int(numPopulation, 1)
+        return(candidateSolution[choosen, x])
+      })
+      candidateSolution[prob] <- Xrm
+    }
+    
+    # Mutation
+    CSFitness <- calcFitness(FUN, optimType, candidateSolution)
+    best <- candidateSolution[order(CSFitness)[1], ]
+    worst <- candidateSolution[order(CSFitness)[numPopulation], ]
+    bestFitness <- calcFitness(FUN, optimType, matrix(best, ncol = numVar))
+    worstFitness <- calcFitness(FUN, optimType, matrix(worst, ncol = numVar))
+    gbest <- calcBest(FUN, -1*optimType, rbind(candidateSolution, gbest))
+    Kibest <- sapply(CSFitness, function(x){
+      kij(x, bestFitness, bestFitness, worstFitness)  
+    })
+    randomMatrix <- matrix(runif(numVar * numPopulation), ncol = numVar)
+    Mu <- 0.05 * Kibest
+    prob <- randomMatrix < Mu
+    if(!all(prob == FALSE)){
+      Xgbest <- sapply(col(candidateSolution)[prob], function(x){
+        P <- sample.int(numPopulation, 1)
+        Q <- sample.int(numPopulation, 1)
+        return(gbest[x] + mu * (candidateSolution[P, x] - candidateSolution[Q, x]))
+      })
+      candidateSolution[prob] <- Xgbest
+    }
+    setTxtProgressBar(progressbar, t)
   }
+  close(progressbar)
+  gbest <- calcBest(FUN, -1*optimType, rbind(candidateSolution, gbest))
+  return(gbest)
 }
 
 
-isIncludeSensingDistances <- function(input){
-  test <- as.matrix(input)
-  distances <- as.matrix(dist(test, diag = TRUE, upper = TRUE))
-  n <- ncol(distances)
-  sensingDistances <- apply(distances, c(1), function(x){
-    1/5*n*sum(x)
-  })
-  return(distances < sensingDistances)
+xij <- function(i, j, epsilon){
+  (j - i)/(dist(rbind(j, i)) + epsilon)
 }
 
-# KHMotion
-KHMotion <- function(i, j, epsilon){
-  # either or both i and j must be matrix with same size
-  if(!is.matrix(i) & !is.matrix(j)){
-    stop("either or both i and j must be matrix with same size")
-  }else if(!is.matrix(i)){
-    i <- matrix(rep(as.numeric(i), nrow(j)), ncol = ncol(j), byrow = TRUE)
-  }else if(!is.matrix(j)){
-    j <- matrix(rep(as.numeric(j), nrow(i)), ncol = ncol(i), byrow = TRUE)
-  }
-  nv <- ncol(i)
-  result <- as.matrix(apply(cbind(i, j), c(1), function(x){
-    indexI <- 1:nv
-    indexJ <- (nv+1):(nv*2)
-    return(x[indexJ] - x[indexI]/dist(rbind(x[indexJ], x[indexI])) + epsilon)
-  }))
-  if(nv > 1){
-    result <- t(result)
-  }
-  result[result == Inf | result == -Inf] <- 0
-  return(result)
-}
-
-# KHFitnessMotion
-KHFitnessMotion <- function(i, j, worst, best){
-  # fitness
-  if(length(i) == 1 & length(j) > 1){
-    i <- rep(as.numeric(i), length(j))
-  }else if(length(i) > 1 & length(j) == 1){
-    j <- rep(as.numeric(j), length(i))
-  }
-  result <- as.numeric(i - j/worst - best)
-  result[is.nan(result)] <- -Inf
-  return(result)
-}
-
-randomMatrix <- function(numberRow, numberColumn, minRange, maxRange){
-  apply(matrix(1:(numberColumn*numberRow), ncol = numberColumn, nrow = numberRow), c(1, 2), function(x){
-    runif(1, min = minRange, max = maxRange)
-  })
+kij <- function(i, j, best, worst){
+  (i - j)/(worst - best)
 }
